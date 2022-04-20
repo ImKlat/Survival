1507750f0bb50d51bcaf19ade59fb54516ba5951
Miquel Angel <hulkrojo2@gmail.com>
2022-04-20 21:56:50 +0200

    Revert "changed spigot to 1.16.5 and fixed compile errors"
    This reverts commit 984b3f8fb7bb8d269d9aec0c4e3f03f4a1fba2db.

--- src/main/java/es/hulk/survival/listeners/EntityListener.java
+++ src/main/java/es/hulk/survival/listeners/EntityListener.java
@@ -7,6 +7,7 @@ import es.hulk.survival.utils.Utils;
 import es.hulk.survival.utils.counter.CounterHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_18_R1.entity.CraftEnderDragon;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
@@ -74,6 +75,16 @@ public class EntityListener implements Listener {
                             event.getDrops().add(new ItemStack(Material.HONEY_BLOCK));
                         }
                     }
+                }
+            }
+        }
+    }
+
+    public void checkEntityDeath(EntityDeathEvent event) {
+        if (Survival.get().isSpeedRun()) {
+            if (event.getEntity() instanceof CraftEnderDragon) {
+                for (Player online : Bukkit.getOnlinePlayers()) {
+                    online.sendTitle(Utils.color("&c&lEL ENDERDRAGON HA MUERTO"), Utils.color("&aHas necesitado &e" + TimeUtil.convert(CounterHelper.getTimer().getSeconds()) + " &apara matarlo"), 0, 20, 20);
                 }
             }
         }
