439c60f942427f0c44ca0e0e8af65f10719d8fcc
Miquel Angel <hulkrojo2@gmail.com>
2022-08-07 18:17:08 +0200

    a

--- src/main/java/es/hulk/survival/listeners/PlayerListener.java
+++ src/main/java/es/hulk/survival/listeners/PlayerListener.java
@@ -1,11 +1,14 @@
 package es.hulk.survival.listeners;
 
 import es.hulk.survival.utils.Utils;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerListener implements Listener {
 
@@ -19,5 +22,17 @@ public class PlayerListener implements Listener {
     public void onPlayerLeave(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         event.setQuitMessage(Utils.color("&cEl cabron de &a" + player.getDisplayName() + " &ca dejado de tocar los cojones."));
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        if (player.getGameMode().equals(GameMode.SPECTATOR)) player.setGameMode(GameMode.SURVIVAL);
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        if (player.getGameMode().equals(GameMode.SPECTATOR)) player.setGameMode(GameMode.SURVIVAL);
     }
 }
